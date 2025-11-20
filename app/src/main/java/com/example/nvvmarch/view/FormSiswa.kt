3fbd491b1b9b6053b9fd2b781b493cab004e8863
sekarasriii <sekareeesh@gmail.com>
2025-11-20 16:21:35 +0700

    menambahkan garis antara pilihan jenis kelamin dan alamat

--- app/src/main/java/com/example/nvvmarch/view/FormSiswa.kt
+++ app/src/main/java/com/example/nvvmarch/view/FormSiswa.kt
@@ -102,6 +102,14 @@ fun FormSiswa(
                 }
             }
 
+            HorizontalDivider(
+                modifier = Modifier
+                    .padding(20.dp)
+                    .width(250.dp),
+                thickness = 1.dp,
+                color = Color.Red
+            )
+
 
 
 
