ab9567c465febb4ab0f8d3c97ad109d55aea5437
sekarasriii <sekareeesh@gmail.com>
2025-11-20 16:42:59 +0700

    menambahkan variabel dan mengedit pair nama lengkap, jenis kelamin dan alamat

---
+++ app/src/main/java/com/example/nvvmarch/view/TampilSiswa.kt
@@ -0,0 +1,40 @@
+package com.example.nvvmarch.view
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.nvvmarch.R
+import com.example.nvvmarch.model.Siswa
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TampilSiswa(
+    statusUiSiswa: Siswa,
+    onBackBtnClick: () -> Unit
+) {
+    val items = listOf(
+        Pair(stringResource("Nama Lengkap"), statusUiSiswa.nama),
+        Pair(stringResource("Jenis Kelamin"), statusUiSiswa.gender),
+        Pair(stringResource("Alamat"), statusUiSiswa.alamat)
+    )
+
