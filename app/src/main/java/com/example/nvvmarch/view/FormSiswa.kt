5a97f1dd605870ef5a645186b8e44628106e2289
sekarasriii <sekareeesh@gmail.com>
2025-11-20 15:49:45 +0700

    menambahkan function FormSiswa dan parameter pilihanJK, onSubmitButtonClicked

---
+++ app/src/main/java/com/example/nvvmarch/view/FormSiswa.kt
@@ -0,0 +1,14 @@
+package com.example.nvvmarch.view
+
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun FormSiswa(
+    pilihanJK: List<String>,
+    obSubmitButtonClicked: (MutableList<String>) -> Unit,
+    modifier: Modifier = Modifier
+) {
+
+)
