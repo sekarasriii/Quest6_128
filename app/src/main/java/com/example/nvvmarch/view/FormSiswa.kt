ba19abe3bfdfe248e0b1636f17c9d7db58b24190
sekarasriii <sekareeesh@gmail.com>
2025-11-20 15:52:41 +0700

    menambahkan 4 variable nama, alamat, gender dan listData

--- app/src/main/java/com/example/nvvmarch/view/FormSiswa.kt
+++ app/src/main/java/com/example/nvvmarch/view/FormSiswa.kt
@@ -2,6 +2,11 @@ package com.example.nvvmarch.view
 
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.ui.Modifier
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.getValue
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -10,5 +15,10 @@ fun FormSiswa(
     obSubmitButtonClicked: (MutableList<String>) -> Unit,
     modifier: Modifier = Modifier
 ) {
+    var txtNama by rememberSaveable { mutableStateOf("") }
+    var txtAlamat by remember { mutableStateOf("") }
+    var txtGender by remember { mutableStateOf("") }
+    val listData: MutableList<String> = mutableListOf(txtNama, txtAlamat, txtGender)
 
+}
 )
