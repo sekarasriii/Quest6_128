acb333761e1d8328a20248169cb0b10fc8c431ec
sekarasriii <sekareeesh@gmail.com>
2025-11-20 15:42:49 +0700

    menambahkan function untuk setSiswa

--- app/src/main/java/com/example/nvvmarch/viewModel/SiswaViewModel.kt
+++ app/src/main/java/com/example/nvvmarch/viewModel/SiswaViewModel.kt
@@ -11,5 +11,9 @@ class SiswaViewModel : ViewModel {
     private val _statusUI = MutableStateFlow( Siswa())
     val statusUI : StateFlow<Siswa> = _statusUI.asStateFlow()
 
-
+    fun setSiswa(ls:MutableList<String>) {
+        _statusUI.update { statusSaatIni ->
+            statusSaatIni.copy(nama=ls[0], gender=ls[1], alamat=ls[2])
+        }
+    }
 }
